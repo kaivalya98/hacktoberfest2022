a68eb8d343a580783bf36cbb1e0ca28282cbbd28
cy20001yadav <cy20001yadav@gmail.com>
2022-09-25 13:34:51 +0530

    hacktoberfest2022
    Signed-off-by: cy20001yadav <cy20001yadav@gmail.com>

---
+++ Python/even or odd.py
@@ -0,0 +1,6 @@
+#simple way of finding odd and even.
+num = int(input("Enter any integer number: ")) #num variable to store any integer number
+if num&1:
+  print("Num is Odd")
+else:
+  print("Num is Even")
