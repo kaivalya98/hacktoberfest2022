9d472dfcd82077cc460b2894268c70d6764e0875
kaivalya98 <107976165+kaivalya98@users.noreply.github.com>
2022-10-01 18:43:45 +0530

    Update HeapSort.java

--- Java/HeapSort.java
+++ Java/HeapSort.java
@@ -50,14 +50,8 @@ public class HeapSort
         } 
     } 
 
-    /* A utility function to print array of size n */
-    static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i]+" "); 
-        System.out.println(); 
-    } 
+   
+   
 
     // Driver program 
     public static void main(String args[]) 
