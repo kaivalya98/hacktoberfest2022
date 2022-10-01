63d137889b7a728657d94d69581b57bc869e1552
Shanatnu Raut <rshantanu73@gmail.com>
2022-10-01 15:45:59 +0530

    complete celling question from binary search

--- Java/binarySearchQuestion/celling.java
+++ Java/binarySearchQuestion/celling.java
@@ -2,23 +2,38 @@ package com.binarySearchQuestion;
 
 public class celling {
     public static void main(String[] args) {
-        // brutal force high time complexity
-//        int arr[]={2,3,5,9,14,16,18};
-//        int target = 11;
-//        int c =0;
-//        int ans = 1000000;
-//
-//        for (int i = 0; i < arr.length ; i++) {
-//            if (target<=arr[i]){
-//                c = arr[i];
-//                if(c<ans){
-//                    ans = c;
-//                }
-//            }
-//        }
-//        System.out.println(ans);
+//         brutal force high time complexity
+        int arr[]={2,3,5,9,14,16,18};
+        int target = 11;
+        int ans = ceiling(arr, target);
+        System.out.println(ans);
+    }
+static int ceiling(int[] arr, int target) {
+        if(target > arr[arr.length-1]){
+            return -1;
+        }
+        int start = 0;
+        int end = arr.length - 1;
 
-        // optimised solution
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+        return start;
 
     }
 }
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 99;
+        int ans = ceiling(arr, target);
+        System.out.println(ans);
+    }
