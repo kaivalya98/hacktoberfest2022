d441ce974333d32e0926f8f2cfdc7a5fe72b9c67
Salini Mahapatra <114807312+Salinimahapatra@users.noreply.github.com>
2022-10-01 18:49:54 +0530

    Update HeapSort.java

--- Java/HeapSort.java
+++ Java/HeapSort.java
@@ -50,8 +50,14 @@ public class HeapSort
         } 
     } 
 
-   
-   
+    /* A utility function to print array of size n */
+    static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i]+" "); 
+        System.out.println(); 
+    } 
 
     // Driver program 
     public static void main(String args[]) 
@@ -65,4 +71,5 @@ public class HeapSort
         System.out.println("Sorted array is"); 
         printArray(arr); 
     } 
-} 
+}
+
